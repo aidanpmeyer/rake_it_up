245f0c58427d43596ada7c4295028f4be70b7648
Marrin Nerenberg <marrinnerenberg@berkeley.edu>
2023-06-02 11:45:09 -0700

    kinda fixed climbing issue.... a lil funky

--- Rake it up/Assets/scripts/PlayerController.cs
+++ Rake it up/Assets/scripts/PlayerController.cs
@@ -8,12 +8,29 @@ public class PlayerController : MonoBehaviour
     public CharacterController controller;
 
     public float speed = 12f;
+    public float gravity = -9.81f;
+
+    Vector3 velocity;
+
+    public Transform groundCheck;
+    public float groundDistance = 0.4f;
+    public LayerMask groundMask;
+    bool isGrounded;
 
     void Update()
     {
+        isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
+
+        if (isGrounded && velocity.y < 0) {
+            velocity.y = -2f;
+            Debug.Log("Velocity Reset");
+        }
         float x = Input.GetAxis("Horizontal");
         float z = Input.GetAxis("Vertical");
         Vector3 move = transform.right * x + transform.forward * z;
         controller.Move(move * speed * Time.deltaTime);
+
+        velocity.y += gravity * Time.deltaTime;
+        controller.Move(velocity * Time.deltaTime);
     }
 }
